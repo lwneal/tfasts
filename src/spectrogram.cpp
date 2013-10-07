18b639e6fdd0f3306286c93d6e9fb6a564667a49
Lawrence Neal <nealla@cocoa.local>
2013-10-06 20:07:42 -0700

    Now outputting scaled, filtered spectrograms

--- src/spectrogram.cpp
+++ src/spectrogram.cpp
@@ -13,16 +13,6 @@ typedef dlib::cmd_line_parser<char>::check_1a_c ArgParser;
 typedef dlib::array2d<dlib::rgb_pixel> ImageRGB;
 typedef dlib::rgb_pixel Pixel;
 
-pRandomForest makeTree() {
-  int num_trees = 10;
-  int num_classes = 2;
-  int feature_dim = 4;
-
-  vector<pExample> training;
-  pRandomForest *rf = new pRandomForest(num_trees, num_classes, feature_dim, training);
-  return *rf;
-}
-
 void spectrogram_parse_args(ArgParser &parser, int argc, char *argv[]) {
 	parser.add_option("h", "Display this help message");
 	parser.add_option("i", "An input .wav audio file", 1);
@@ -36,25 +26,42 @@ void spectrogram_parse_args(ArgParser &parser, int argc, char *argv[]) {
 	}
 }
 
-int main(int argc, char *argv[]) {
-	ArgParser args;
-	spectrogram_parse_args(args, argc, argv);
-
-	cout << "Calling args.option.argument" << endl;
-	string fn_in = args.option("i").argument();
-	string fn_out = args.option("o").argument();
-
-	cout << "about to create a spectrogram from input " << fn_in << endl;
-	Mask spec(fn_in, 512, 256);
-
-	cout << "Saving image size " << spec.width() << "," << spec.height() << " to " << fn_out << endl;
+void save_img(Mask &spec, string filename) {
+	cout << "Saving image size " << spec.width() << "," << spec.height() << " to " << filename << endl;
 
 	ImageRGB save_img(spec.height(), spec.width());
 	spec.foreach([&](int x, int y) {
 		double val = 255.0 * spec.at(x,y);
 		save_img[y][x] = Pixel(val,val,val);
 	});
-	dlib::save_bmp(save_img, fn_out);
+	dlib::save_bmp(save_img, filename);
+}
 
+int main(int argc, char *argv[]) {
+	ArgParser args;
+	spectrogram_parse_args(args, argc, argv);
+
+	string fn_in = args.option("i").argument();
+	string fn_out = args.option("o").argument();
+
+	int fft_width = 512;
+	int fft_step = 256;
+	int band_pass_px = 12;
+
+	Mask spec(fn_in, 512, 256);
+
+	spec = Mask(spec.width(), spec.height(), [&](int x, int y) {
+		return y < band_pass_px ? 0 : spec(x,y);
+	});
+
+	spec = spec.whitening_filter();
+
+	spec = spec.div_by(spec.get_max());
+
+	spec = Mask(spec.width(), spec.height(), [&](int x, int y) {
+		return sqrt(spec(x,y));
+	});
+
+	save_img(spec, fn_out);
 	return 0;
 }
