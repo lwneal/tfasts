2356a1d15c3678d07625e4548668350d97f021cd
Lawrence Neal <nealla@cocoa.local>
2013-10-06 19:45:57 -0700

    Basic output of unfiltered, unscaled spectrogram images

--- Makefile
+++ Makefile
@@ -1,20 +1,22 @@
 CC=clang++ -std=c++11 -stdlib=libc++
 CFLAGS=-g
 
+.PHONY: all test clean dlib
+
 all: spectrogram
 
-spectrogram: src/spectrogram.cpp Mask.o pRandomForest.o
-	${CC} ${CFLAGS} *.o src/spectrogram.cpp -I include -o bin/spectrogram
-
-Mask.o: include/Mask.h src/Mask.cpp dlib Grid.o
-	${CC} ${CFLAGS} -I include -c src/Mask.cpp
-
-Grid.o: include/Grid.h src/Grid.cpp dlib
-	${CC} ${CFLAGS} -I include -c src/Grid.cpp
+test: all
+	bin/spectrogram -i hja_birds/wavs/PC7_20090704_090000_0040.wav -o test.bmp
 
 dlib: source.o
 source.o: include/dlib/all/source.cpp
 	${CC} ${CFLAGS} -DDLIB_NO_GUI_SUPPORT -I include -c include/dlib/all/source.cpp
+
+Mask.o: include/Mask.h src/Mask.cpp source.o Grid.o
+	${CC} ${CFLAGS} -I include -c src/Mask.cpp
+
+Grid.o: include/Grid.h src/Grid.cpp source.o
+	${CC} ${CFLAGS} -I include -c src/Grid.cpp
 
 FFT.o: src/FFT.cpp include/FFT.h
 	${CC} ${CFLAGS} -I include -c src/FFT.cpp
@@ -24,6 +26,9 @@ pRFDecisionTree.o: include/pRFDecisionTree.h src/pRFDecisionTree.cpp
 
 pRandomForest.o: include/pRandomForest.h src/pRandomForest.cpp pRFDecisionTree.o FFT.o
 	${CC} ${CFLAGS} -I include -c src/pRandomForest.cpp
+
+spectrogram: src/spectrogram.cpp Mask.o pRandomForest.o
+	${CC} ${CFLAGS} *.o src/spectrogram.cpp -I include -o bin/spectrogram
 
 clean:
 	rm -rf *.o bin/*
--- src/spectrogram.cpp
+++ src/spectrogram.cpp
@@ -2,6 +2,7 @@
 #include "Mask.h"
 
 #include "dlib/cmd_line_parser.h"
+#include "dlib/image_io.h"
 
 #include <string>
 #include <iostream>
@@ -9,6 +10,8 @@
 using namespace std;
 
 typedef dlib::cmd_line_parser<char>::check_1a_c ArgParser;
+typedef dlib::array2d<dlib::rgb_pixel> ImageRGB;
+typedef dlib::rgb_pixel Pixel;
 
 pRandomForest makeTree() {
   int num_trees = 10;
@@ -23,7 +26,7 @@ pRandomForest makeTree() {
 void spectrogram_parse_args(ArgParser &parser, int argc, char *argv[]) {
 	parser.add_option("h", "Display this help message");
 	parser.add_option("i", "An input .wav audio file", 1);
-	parser.add_option("o", "Filename for output .bmp spectrogram");
+	parser.add_option("o", "Filename for output .bmp spectrogram",1);
 	parser.parse(argc, argv);
 
 	if (parser.option("h") || !parser.option("o")) {
@@ -37,12 +40,21 @@ int main(int argc, char *argv[]) {
 	ArgParser args;
 	spectrogram_parse_args(args, argc, argv);
 
+	cout << "Calling args.option.argument" << endl;
 	string fn_in = args.option("i").argument();
 	string fn_out = args.option("o").argument();
 
-	Mask spec(fn_in);
+	cout << "about to create a spectrogram from input " << fn_in << endl;
+	Mask spec(fn_in, 512, 256);
 
 	cout << "Saving image size " << spec.width() << "," << spec.height() << " to " << fn_out << endl;
+
+	ImageRGB save_img(spec.height(), spec.width());
+	spec.foreach([&](int x, int y) {
+		double val = 255.0 * spec.at(x,y);
+		save_img[y][x] = Pixel(val,val,val);
+	});
+	dlib::save_bmp(save_img, fn_out);
 
 	return 0;
 }
