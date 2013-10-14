f6d82ee0329094d44f9d857cab0079413b207344
Lawrence Neal <lawrencewneal@gmail.com>
2013-10-14 01:35:05 -0700

    Update ICASSP repro script

--- reproduce_icassp_2011.sh
+++ reproduce_icassp_2011.sh
@@ -1,21 +1,24 @@
 #!/bin/sh
 
-if [[ $# -lt 3 ]]
+if [[ $# -lt 4 ]]
 then
-	echo "Usage: ./reproduce_icassp.sh path/to/hja_birds/wavs/ path/to/hja_birds/labels output/path/"
+	echo "Usage: ./reproduce_icassp.sh path/to/training/wavs/ path/to/hja_birds/labels path/to/testing/wavs/  output/path/"
+	echo
+	echo "For 2-fold validation:"
+	echo "./reproduce_icassp.sh setA/wavs/ setA/labels/ setB/wavs/ output/setB/"
+	echo "./reproduce_icassp.sh setB/wavs/ setB/labels/ setA/wavs/ output/setA/"
 	exit 1
 fi
 
+make
+
 echo "Found `ls $1 | wc -l` wav files and `ls $2 | wc -l` label files: loading..."
 
-if [[ -n icassp_model.rf ]]
-then
-	# Learn a model with the same parameters used for the 2011 ICASSP paper
-	bin/learn -i $1 -l $2 -o icassp_model.rf -n 10 -e 50 -c 0.10
-fi
+# Learn a model from one set of .wav recordings (with labels)
+bin/learn -i $1 -l $2 -o icassp_model.rf -n 10 -e 50 -c 0.10
 
-# Apply the learned model
-for file in `ls $1 | sed s/.wav$//`
+# Apply the learned model to another set of .wav recordings
+for file in `ls $3 | sed s/.wav$//`
 do
-	bin/filter -i $1/${file}.wav -o ${file}.filtered.bmp -a ${file}.bmp
+	bin/filter -i $3/${file}.wav -o $4/${file}.filtered.wav -a $4/${file}.bmp -m icassp_model.rf
 done
