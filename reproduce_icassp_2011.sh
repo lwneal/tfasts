acd91869c06125438f65eeef8ddf1896c6ef6835
Lawrence Neal <lawrencewneal@gmail.com>
2013-10-14 01:50:15 -0700

    Increase # trees and examples
    Will take a long time to run

--- reproduce_icassp_2011.sh
+++ reproduce_icassp_2011.sh
@@ -7,6 +7,7 @@ then
 	echo "For 2-fold validation:"
 	echo "./reproduce_icassp.sh setA/wavs/ setA/labels/ setB/wavs/ output/setB/"
 	echo "./reproduce_icassp.sh setB/wavs/ setB/labels/ setA/wavs/ output/setA/"
+	echo "Note: Will take a LONG TIME to run at full quality! Decrease -n and -e parameters to test"
 	exit 1
 fi
 
@@ -15,7 +16,7 @@ make
 echo "Found `ls $1 | wc -l` wav files and `ls $2 | wc -l` label files: loading..."
 
 # Learn a model from one set of .wav recordings (with labels)
-bin/learn -i $1 -l $2 -o icassp_model.rf -n 10 -e 50 -c 0.10
+bin/learn -i $1 -l $2 -o icassp_model.rf -n 40 -e 500 -c 0.10
 
 # Apply the learned model to another set of .wav recordings
 for file in `ls $3 | sed s/.wav$//`
