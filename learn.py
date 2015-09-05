32a08e0837837aac3f5406caf79d81f3fe99d600
Lawrence Neal <nealla@lwneal.com>
2015-09-05 16:14:20 -0700

    Split convnet code into its own module

--- learn.py
+++ learn.py
@@ -31,7 +31,6 @@ References:
 """
 import cPickle
 import docopt
-import gzip
 import numpy
 import os
 import random
@@ -45,88 +44,10 @@ from logistic_sgd import LogisticRegression
 from logistic_sgd import sgd_optimization_mnist
 from multilayer_perceptron import HiddenLayer
 from multilayer_perceptron import test_mlp
-from theano.tensor.nnet import conv
-from theano.tensor.signal import downsample
+from convnet import LeNetConvPoolLayer
+
 
 BATCH_SIZE=500
-
-
-class LeNetConvPoolLayer(object):
-    """Pool Layer of a convolutional network """
-
-    def __init__(self, rng, input, filter_shape, image_shape, poolsize=(2, 2)):
-        """
-        Allocate a LeNetConvPoolLayer with shared variable internal parameters.
-
-        :type rng: numpy.random.RandomState
-        :param rng: a random number generator used to initialize weights
-
-        :type input: theano.tensor.dtensor4
-        :param input: symbolic image tensor, of shape image_shape
-
-        :type filter_shape: tuple or list of length 4
-        :param filter_shape: (number of filters, num input feature maps,
-                              filter height, filter width)
-
-        :type image_shape: tuple or list of length 4
-        :param image_shape: (batch size, num input feature maps,
-                             image height, image width)
-
-        :type poolsize: tuple or list of length 2
-        :param poolsize: the downsampling (pooling) factor (#rows, #cols)
-        """
-
-        assert image_shape[1] == filter_shape[1]
-        self.input = input
-
-        # there are "num input feature maps * filter height * filter width"
-        # inputs to each hidden unit
-        fan_in = numpy.prod(filter_shape[1:])
-        # each unit in the lower layer receives a gradient from:
-        # "num output feature maps * filter height * filter width" /
-        #   pooling size
-        fan_out = (filter_shape[0] * numpy.prod(filter_shape[2:]) /
-                   numpy.prod(poolsize))
-        # initialize weights with random weights
-        W_bound = numpy.sqrt(6. / (fan_in + fan_out))
-        self.W = theano.shared(
-            numpy.asarray(
-                rng.uniform(low=-W_bound, high=W_bound, size=filter_shape),
-                dtype=theano.config.floatX
-            ),
-            borrow=True
-        )
-
-        # the bias is a 1D tensor -- one bias per output feature map
-        b_values = numpy.zeros((filter_shape[0],), dtype=theano.config.floatX)
-        self.b = theano.shared(value=b_values, borrow=True)
-
-        # convolve input feature maps with filters
-        conv_out = conv.conv2d(
-            input=input,
-            filters=self.W,
-            filter_shape=filter_shape,
-            image_shape=image_shape
-        )
-
-        # downsample each feature map individually, using maxpooling
-        pooled_out = downsample.max_pool_2d(
-            input=conv_out,
-            ds=poolsize,
-            ignore_border=True
-        )
-
-        # add the bias term. Since the bias is a vector (1D array), we first
-        # reshape it to a tensor of shape (1, n_filters, 1, 1). Each bias will
-        # thus be broadcasted across mini-batches and feature map
-        # width & height
-        self.output = T.tanh(pooled_out + self.b.dimshuffle('x', 0, 'x', 'x'))
-
-        # store parameters of this layer
-        self.params = [self.W, self.b]
-
-        # keep track of model input
-        self.input = input
 
 
 def evaluate_lenet5(train_set_x, train_set_y, valid_set_x, valid_set_y, test_set_x, test_set_y,
@@ -467,6 +388,8 @@ if __name__ == '__main__':
             derp = numpy.concatenate([digit] * BATCH_SIZE)
             return [lenet_classifier([derp])[0]]
         lenet_label = underp_lenet(digit)
+	if sgd_label == mlp_label == lenet_label:
+	  continue
 
         print "Logistic regression predicts {0}, MLP predicts {1}, lenet predicts {2}".format(sgd_label, mlp_label, lenet_label)
         print_digit(digit)
