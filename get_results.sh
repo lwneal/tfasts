8a86a72b7b1463f91ae3259be2a21ab7b43216ca
Lawrence Neal <nealla@lwneal.com>
2015-10-11 21:06:46 -0700

    Run this script on the laptop to pull results from the server

---
+++ get_results.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+if [ $# -lt 1 ]; then
+    echo "Usage: $0 TAG"
+    echo "TAG: any mnemonic tag for this run"
+    exit
+fi
+
+IP=52.88.104.238
+TAG=$1
+NAME=`date "+%Y_%m_%d"`_$TAG
+
+for DIR in output demos comparisons; do
+    mkdir ${DIR}_${NAME}
+    scp -r ${IP}:ml-demo/${DIR}/* ${DIR}_${NAME}
+done
+
+python eval.py output_$NAME
