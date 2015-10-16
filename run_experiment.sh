b81ab54747f2c2fd90e51708a79115430217e600
Lawrence Neal <nealla@lwneal.com>
2015-10-16 23:04:19 +0000

    Don't delete demo wav files

--- run_experiment.sh
+++ run_experiment.sh
@@ -2,5 +2,5 @@
 
 rm output/*
 rm comparisons/*
-rm demos/*
+rm demos/*.png
 THEANO_FLAGS=device=gpu,floatX=float32 python birds.py --wavs=setA/wavs/ --labels=setA/labels --unlabeled=setB/wavs/ --epochs=500 --output=output
