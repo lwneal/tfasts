2b6118153240796c1bcb089001b033295760ea3b
Lawrence Neal <nealla@lwneal.com>
2015-10-18 06:34:35 +0000

    Delete png files only

--- run_experiment.sh
+++ run_experiment.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-rm output/*
-rm comparisons/*
+rm kernels/*.png
+rm output/*.png
+rm comparisons/*.png
 rm demos/*.png
 THEANO_FLAGS=device=gpu,floatX=float32 python birds.py --wavs=setA/wavs/ --labels=setA/labels --unlabeled=setB/wavs/ --epochs=500 --output=output
