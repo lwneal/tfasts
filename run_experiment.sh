d978fdc6d8697a1de40725296bfde8ca4f60cf4a
Lawrence Neal <nealla@lwneal.com>
2015-10-18 20:03:10 +0000

    More epochs (6x12 trains much faster than 16x16)

--- run_experiment.sh
+++ run_experiment.sh
@@ -4,4 +4,4 @@ rm kernels/*.png
 rm output/*.png
 rm comparisons/*.png
 rm demos/*.png
-THEANO_FLAGS=device=gpu,floatX=float32 python birds.py --wavs=setA/wavs/ --labels=setA/labels --unlabeled=setB/wavs/ --epochs=500 --output=output
+THEANO_FLAGS=device=gpu,floatX=float32 python birds.py --wavs=setA/wavs/ --labels=setA/labels --unlabeled=setB/wavs/ --epochs=1200 --output=output
